2a88f09b9c8b5b26ba7b80c5ba772bb8c2ca1adf
Nkosinathi Magagula <nkosinathiwalter001@gmail.com>
2026-08-22 22:29:52 +0200

    rust saturday ®️

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,123 @@
-fn main() {
-    println!("Hello, rust!");
+use std::error::Error;
+
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::{TcpListener, TcpStream};
+
+
+#[derive(Debug, PartialEq)]
+enum Command {
+  Ping,
+  Get { key: String },
+  Set { key: String, val: String },
+  Del { key: String },
+}
+
+impl Command {
+  fn from_line(line: &str) -> Result<Command, String> {
+    let parts: Vec<&str> = line.trim().split_whitespace().collect();
+    
+    if parts.is_empty() {
+      return Err("Empty command".to_string());
+    }
+    
+    let cmd_name = parts[0].to_uppercase();
+
+    match cmd_name.as_str() {
+      "PING" => Ok(Command::Ping),
+      "GET" => {
+        if parts.len() == 2 {
+          Ok(
+            Command::Get {
+              key: parts[1].to_string()
+            }
+          )
+        } else {
+            return Err("Usage: GET <key>".to_string());
+        }
+      }
+      "SET" => {
+        if parts.len() == 3 {
+          let val = parts[2..].join(" ");
+
+          Ok(
+            Command::Set {
+              key: parts[1].to_string(),
+              val,
+            }
+          )
+        } else {
+          return Err("Usage: Set <key> <value>".to_string());
+        }
+      }
+      "DEL" => {
+        if parts.len() == 2 {
+          Ok(
+            Command::Del {
+              key: parts[1].to_string()
+            }
+          )
+        } else {
+          return Err("Usage: DEL <key>".to_string());
+        }
+      }
+      _ => Err(format!("Unknown command: '{}'", parts[0])),
+    }
+  }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+  let addr = "127.0.0.1:8765";
+  // let addr = "localhost:8765";
+  let listener = TcpListener::bind(addr).await?;
+
+  println!("Server listening on {}", addr);
+
+  loop {
+    print!("> ");
+    
+    // Accept incoming TCP connections
+    let (socket, client_addr) = listener.accept().await?;
+    println!("New client connected: {}", client_addr);
+
+    // Spawn a new task for each client connection
+    tokio::spawn(async move {
+      if let Err(e) = handle_client(socket).await {
+        eprintln!("Error handling client {}: {:?}", client_addr, e);
+      }
+
+      println!("Client disconnected: {}", client_addr);
+    });
+  }
+}
+
+async fn handle_client(mut socket: TcpStream) -> Result<(), Box<dyn Error>> {
+  let (reader, mut writer) = socket.split();
+  let mut buf_reader = BufReader::new(reader);
+  let mut line = String::new();
+
+  // Read lines until client disconnects
+  while buf_reader.read_line(&mut line).await? > 0 {
+    let response = match Command::from_line(&line) {
+      Ok(cmd) => match cmd {
+        Command::Ping => "+PONG\r\n".to_string(),
+        Command::Set { key, val } => {
+          format!("+OK (parsed SET key='{}' val='{}')\r\n", key, val)
+        },
+        Command::Get { key } => {
+          format!("+OK (parsed GET key='{}')\r\n", key)
+        },
+        Command::Del { key } => {
+          format!("+OK (parsed DEL key='{}')\r\n", key)
+        },
+      }
+
+      Err(err) => format!("-ERR {}\r\n", err),
+    };
+
+    writer.write_all(response.as_bytes()).await?;
+    line.clear();
+  }
+
+  Ok(())
 }
